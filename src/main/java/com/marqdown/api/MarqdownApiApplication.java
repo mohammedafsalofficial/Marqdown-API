4423ddc0691bb6ef6db4aa168c15d3d3e8f47feb
Mohammed Afsal <mohammedafsaltut@gmail.com>
2025-04-12 19:14:51 +0530

    initial commit

---
+++ src/main/java/com/marqdown/api/MarqdownApiApplication.java
@@ -0,0 +1,13 @@
+package com.marqdown.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarqdownApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarqdownApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marqdown/api/MarqdownApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marqdown.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarqdownApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
