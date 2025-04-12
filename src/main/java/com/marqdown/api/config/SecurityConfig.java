90fb8fdb5eb20d98920f820c5b3f7fc5c6f66f38
Mohammed Afsal <mohammedafsaltut@gmail.com>
2025-04-12 21:03:05 +0530

    Basic security config setup

---
+++ src/main/java/com/marqdown/api/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.marqdown.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers("/api/auth/**").permitAll()
+                                .anyRequest().authenticated())
+                .build();
+    }
+}
---
+++ src/main/java/com/marqdown/api/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.marqdown.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register() {
+        return new ResponseEntity<>("User Registered Successfully.", HttpStatus.OK);
+    }
+}
